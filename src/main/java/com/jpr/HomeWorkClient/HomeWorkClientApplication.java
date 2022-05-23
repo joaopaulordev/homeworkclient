1d513bd897dcd62f38311e346b8a8b27ebacc620
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2022-05-23 19:16:11 -0300

    Project Created

---
+++ src/main/java/com/jpr/HomeWorkClient/HomeWorkClientApplication.java
@@ -0,0 +1,13 @@
+package com.jpr.HomeWorkClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpr/HomeWorkClient/HomeWorkClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpr.HomeWorkClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
